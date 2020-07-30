5d28a8c2ddd7aa5aca77f9a8e9cf081e22a6d14f
lively-system-6174 <wkalada1@gmail.com>
2020-07-30 18:21:59 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -2,11 +2,13 @@ require 'pry'
 # don't forget to add: require 'pry'
 
 def start_game(player1, player2)
-  "Hello " + player1 + " & " + playar2
+  start_message = "Hello #{player1} & #{playar2}"
+  start_message
 end
 
 def play_game(player1, player2)
-  "#{player1} is better than #{player2}"
+  play_message = "#{player1} is better than #{player2}"
+  play_message
 end
 
 def runner
